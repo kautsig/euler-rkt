a3bde910fb5d2d6e4bea8f035a43175791753e8a
Chris Raschl <cr@kautsig.org>
2017-10-07 20:54:07 +0200

    Add formatting to comments

--- 4.rkt
+++ 4.rkt
@@ -4,8 +4,10 @@
 
 ;; A palindromic number reads the same both ways. The largest
 ;; palindrome made from the product of two 2-digit numbers is 9009 =
-;; 91 × 99. Find the largest palindrome made from the product of two
-;; 3-digit numbers.
+;; 91 × 99.
+
+;; Find the largest palindrome made from the product of two 3-digit
+;; numbers.
 
 (require threading)
 
